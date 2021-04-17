16ec30a3e7428e0e8d288da721ac659d8e95e28c
Logi05 <80442511+Logi05@users.noreply.github.com>
2021-04-17 11:25:32 +0530

    Update MethodEg.java

--- Assessment/src/com/training/MethodEg.java
+++ Assessment/src/com/training/MethodEg.java
@@ -1,5 +1,24 @@
 package com.training;
 
+System.out.println("Using Lambda Expression:");
+
+interface IntSequence1{
+	public int constant(int a);
+}
+public class Method1 {
+	public static void main(String[] args) {
+		IntSequence1 ab=a->{
+			while(true)
+			{
+				System.out.println(a);
+			}
+		};
+		System.out.println(ab.constant(1));
+	}
+}
+
+/*System.out.println("Using Normal Static Method of Class:");
+
 public class MethodEg  {
 	public static void main(String[] args) {
 		IntSequence obj=new IntSequence();
@@ -15,5 +34,5 @@ class IntSequence
 			System.out.println(i);
 		}
 	}
-}
+}*/
 
